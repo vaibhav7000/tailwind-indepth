be2dd84e2f2109897fd30f227cf7dea1031d6a13
vaibhav7000 <vchawla7000@gmail.com>
2025-07-22 21:27:29 +0530

    more tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,26 @@ export default function App() {
         font-size
       </div>
 
+      <div className="text-(length:--my-font-size) italic xl:text-twelve">
+        font-style
+      </div>
+
+      <div className="font-thin text-7xl sm:font-extralight md:font-light xl:font-(--font-400) 2xl:font-extrablack">
+        font-weight
+      </div>
+
+    <div className="text-7xl">
+      <div className="text-base tracking-widest sm:tracking-custom-letter-spacing md:tracking-(--custom-letter-spacing)">
+          Letter-spacing / tracking
+        </div>
+    </div>
+
+    <div className="">
+      <div className="w-[100px] bg-red-200 aspect-square line-clamp-none sm:line-clamp-4 md:line-clamp(--custom-line-clamping)">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ab rem quam, iste debitis voluptate hic at consequuntur, omnis explicabo provident aut, quia laboriosam unde blanditiis maxime corporis asperiores harum.
+      </div>
+    </div>
+
     </div>
   )
 }
