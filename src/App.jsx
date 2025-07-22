6a8385c57ef0421b3a0b0576e96ff246e8729f1e
vaibhav7000 <vchawla7000@gmail.com>
2025-07-22 20:30:26 +0530

    font-family and size with theme-variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex justify-center items-center flex-col">
+
+      <div className="font-sans sm:font-mono md:font-sofia-pro lg:font-serif xl:font-(family-name:--personal-font) 2xl:font-[Nunito]">
+            font-family
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="text-xs sm:text-sm md:text-base lg:text-lg xl:font-[Nunito] xl:text-8xl xl:text-(length:--my-font-size) xl:text-[20px] xl:text-twelve 2xl:text-7xl">
+        font-size
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
-
-export default App
